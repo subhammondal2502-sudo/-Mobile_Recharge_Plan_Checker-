453294b21e053b8cec7f2725cc426ba53c854aea
Subham Mondal <subhammondal2502@gmail.com>
2026-09-23 20:39:47 +0530

    Update Mobile_Recharge_Plan_Checker.py

--- Mobile_Recharge_Plan_Checker.py
+++ Mobile_Recharge_Plan_Checker.py
@@ -6,7 +6,7 @@ print("customer name :",name)
 print("plan type : ", plan_type) 
 print("recharge amount :",amount)
 if plan_type=="basic":
-if amount>=299 :
+    if amount>=299 :
         print("plan status : Activated \n validity : 42 days \n Offer : Extra 5GB Data")
  elif amount>=199 :
         print("plan status : Activated \n validity : 28 days \n Offer : Extra 3GB Data")
