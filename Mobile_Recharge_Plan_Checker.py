6eefbcb827e26334f426b0ed6686fb6e9e0eb436
Subham Mondal <subhammondal2502@gmail.com>
2026-09-23 19:11:06 +0530

    Add invalid standard plan recharge check .

--- Mobile_Recharge_Plan_Checker.py
+++ Mobile_Recharge_Plan_Checker.py
@@ -17,3 +17,5 @@ if amount>=499 :
         print("plan status : Activated \n validity : 70 days \n Offer : Extra 9GB Data") 
     elif amount>=399 :
         print("plan status : Activated \n validity : 56 days \n Offer : Extra 6GB Data")
+    else:
+        print("invalid recharge amount")
