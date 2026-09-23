a39f11771d32db4e51ce3abfbf2782b377711c65
Subham Mondal <subhammondal2502@gmail.com>
2026-09-23 18:56:52 +0530

    Display plan type .

--- Mobile_Recharge_Plan_Checker.py
+++ Mobile_Recharge_Plan_Checker.py
@@ -3,4 +3,4 @@ name=input("enter customer name :")
 plan_type=input("enter plan type-(basic/standard/premium) :")
 amount=int(input("enter recharge amount :"))
 print("customer name :",name)
- 
+print("plan type : ", plan_type) 
