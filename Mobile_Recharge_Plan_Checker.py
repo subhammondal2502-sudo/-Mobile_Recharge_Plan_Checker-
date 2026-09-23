b149c2df2eeac2fb217cb625aaab4f0ea616ab9b
Subham Mondal <subhammondal2502@gmail.com>
2026-09-23 19:00:07 +0530

    Add basic plan amount validation .

--- Mobile_Recharge_Plan_Checker.py
+++ Mobile_Recharge_Plan_Checker.py
@@ -6,3 +6,5 @@ print("customer name :",name)
 print("plan type : ", plan_type) 
 print("recharge amount :",amount)
 if plan_type=="basic":
+if amount>=299 :
+        print("plan status : Activated \n validity : 42 days \n Offer : Extra 5GB Data")
