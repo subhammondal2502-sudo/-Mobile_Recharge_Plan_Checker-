c404f90bae0a40655a516dd8158c22f465c44a96
Subham Mondal <subhammondal2502@gmail.com>
2026-09-23 13:15:41 +0530

    Add files via upload

---
+++ Mobile_Recharge_Plan_Checker.py
@@ -0,0 +1,6 @@
+# Mobile_Recharge_Plan_Checker.....
+name=input("enter customer name :")
+plan_type=input("enter plan type-(basic/standard/premium) :")
+amount=int(input("enter recharge amount :"))
+
+ 
