0e6c4c90ec50d20d724fbd95a3ca158167ba921c
Subham Mondal <subhammondal2502@gmail.com>
2026-09-25 19:05:11 +0530

    Update Mobile_Recharge_Plan_Checker.py

--- Mobile_Recharge_Plan_Checker.py
+++ Mobile_Recharge_Plan_Checker.py
@@ -5,7 +5,6 @@ amount=int(input("enter recharge amount :"))
 print("customer name :",name)
 print("plan type : ", plan_type)
 print("recharge amount :",amount)
-
 if plan_type=="basic":
     if amount>=299 :
         print("plan status : Activated \n validity : 42 days \n Offer : Extra 5GB Data")
@@ -13,7 +12,6 @@ if plan_type=="basic":
         print("plan status : Activated \n validity : 28 days \n Offer : Extra 3GB Data")
     else:
         print("invalid recharge amount")
-        
 elif plan_type=="standard":
     if amount>=499 :
         print("plan status : Activated \n validity : 70 days \n Offer : Extra 9GB Data")  
@@ -21,7 +19,6 @@ elif plan_type=="standard":
         print("plan status : Activated \n validity : 56 days \n Offer : Extra 6GB Data")
     else:
         print("invalid recharge amount")
-        
 elif plan_type=="premium":
     if amount>=799 :
         print("plan status : Activated \n validity : 100 days \n Offer : Extra 16GB Data")  
